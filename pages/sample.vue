d9fadbeebd6b0a62a17e8107b1085df54895003f
audeypx <audreypx100@gmail.com>
2021-05-29 17:51:36 +0100

    fixed toggle

---
+++ pages/sample.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="px-24 py-12">
+    <button class="bg-green-400 py-2 px-4 text-white" @click="add">Add</button>
+    <!-- <button class="bg-green-400 py-2 px-4 text-white" @click="add">Add</button> -->
+
+    <span v-if="count === 1" class="text-2xl"> Drey </span>
+    <span v-if="count === 2" class="text-2xl"> Audrey </span>
+    <span v-if="count === 3" class="text-2xl"> Ozioma </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sample',
+  data() {
+    return {
+      count: 0,
+    }
+  },
+
+  methods: {
+    add() {
+      if (this.count === '') return
+      this.count++
+    },
+  },
+  mounted() {
+    setInterval(this.add, 1500)
+  },
+}
+</script>
